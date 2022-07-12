f1ff898a1c5ae6ac21d3452b4c0eb6a7392a7890
Matteo <matteo.dora@ens.fr>
2022-07-12 18:20:15 +0200

    Set fallback paths

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Where the simulations results will be saved.
-DATA_PATH = Path(os.getenv('ER_DATA_PATH'))
+DATA_PATH = Path(os.getenv("ER_DATA_PATH", "./resources"))
 DATA_PATH.mkdir(parents=True, exist_ok=True)
 
 # Where the generated figures will be saved.
-FIGURES_PATH = Path(os.getenv('ER_FIGURES_PATH'))
+FIGURES_PATH = Path(os.getenv("ER_FIGURES_PATH", "./resources"))
 FIGURES_PATH.mkdir(parents=True, exist_ok=True)
