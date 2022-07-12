64b8c9f7ff990f1a39dfad3c45bba1ad71d25040
Matteo <matteo.dora@ens.fr>
2022-07-12 18:07:13 +0200

    Use dotenv for configuration

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Where the simulations results will be saved.
-DATA_PATH = Path(__file__).parent.joinpath('resources/data')
+DATA_PATH = Path(os.getenv('ER_DATA_PATH'))
 DATA_PATH.mkdir(parents=True, exist_ok=True)
 
 # Where the generated figures will be saved.
-FIGURES_PATH = Path(__file__).parent.joinpath('resources/figures')
+FIGURES_PATH = Path(os.getenv('ER_FIGURES_PATH'))
 FIGURES_PATH.mkdir(parents=True, exist_ok=True)
